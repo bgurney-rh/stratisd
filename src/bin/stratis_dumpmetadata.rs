081e9cacb5aedac8976030444774cd008079bfba
Bryan Gurney <bgurney@redhat.com>
2020-07-06 13:27:26 -0400

    SCRATCH: stratis_dumpmetadata: print struct contents
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- src/bin/stratis_dumpmetadata.rs
+++ src/bin/stratis_dumpmetadata.rs
@@ -1,9 +1,14 @@
 use std::env;
 
-// use libstratis::engine::strat_engine::backstore::metadata::BDA;
+use std::fs::OpenOptions;
+
+use libstratis::engine::strat_engine::backstore::metadata::BDA;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let devpath = args[1].clone();
-    println!("{}", devpath)
+    println!("Device path: {}", devpath);
+
+    let bda = BDA::load(&mut OpenOptions::new().read(true).open(&devpath).unwrap()).unwrap();
+    println!("{:?}", bda)
 }

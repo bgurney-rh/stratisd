7fb67d3276e7b889ee8bf52242a93b39b89407fb
Bryan Gurney <bgurney@redhat.com>
2020-07-07 14:39:38 -0400

    SCRATCH: add load_state call

--- src/bin/stratis_dumpmetadata.rs
+++ src/bin/stratis_dumpmetadata.rs
@@ -2,6 +2,7 @@ use std::env;
 
 use std::fs::OpenOptions;
 use std::io::{Seek, SeekFrom};
+use std::vec::Vec;
 
 use libstratis::engine::strat_engine::backstore::metadata::BDA;
 
@@ -12,8 +13,12 @@ fn main() {
 
     let mut devfile = OpenOptions::new().read(true).open(&devpath).unwrap();
 
-    let bda = BDA::load(&mut devfile);
+    let bda = BDA::load(&mut devfile).unwrap().unwrap();
     println!("{:#?}", bda);
 
     devfile.seek(SeekFrom::Start(0)).unwrap();
+
+    //let mut buf: Vec<u8> = Vec::new();
+    let loaded_state = bda.load_state(&mut devfile).unwrap();
+    println!("{:x?}", loaded_state);
 }
